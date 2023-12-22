f84fd34f0f7053d056007f6f9f9b2e3aadb25d0f
Guariko <creatingsomething2021@gmail.com>
2023-12-22 07:05:11 -0300

    Setting up Project

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,9 @@
+export default function Main() {
+  return (
+    <main>
+      <section>
+        <h1 className="text-6xl text-center text-blue-400">Hello world</h1>
+      </section>
+    </main>
+  );
+}
